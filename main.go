5b85d8db12b85793857f0baa99dc3f861f17694d
printf <printfpk@gmail.com>
2025-11-15 15:27:40 +0530

    first commit

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+import "fmt"
+
+func main() {
+	var conferenceName string = "Go Conference"
+	const conferenceTickets int = 50
+	var remainingTickets uint = 50
+	fmt.Println("welcome to", conferenceName, "booking application")
+	fmt.Println("we have total", conferenceTickets, "tickets and", remainingTickets, "are still left")
+	fmt.Println("Get your tickets to attend")
+
+	
+	
+}
